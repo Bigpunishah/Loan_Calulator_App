6e0e47f3631f159b24b39f3c36bd25da40e9a66e
Jay C <vikel965@hotmail.com>
2024-04-23 11:32:26 -0400

    Just added the final portions to the loan_calculator.xml view. Now working on the .java file associated with it.

--- app/src/main/java/com/example/simplemoneycalculator/LoanCalculatorActivity.java
+++ app/src/main/java/com/example/simplemoneycalculator/LoanCalculatorActivity.java
@@ -3,8 +3,29 @@ package com.example.simplemoneycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class LoanCalculatorActivity extends AppCompatActivity {
+
+    //Create all variables that will be associated with the activity
+    private EditText loanAmountEditText;
+    private EditText loanTermYearsEditText;
+    private EditText loanTermMonthsEditText;
+    private EditText interestRateEditText;
+    private Spinner payRateSpinner;
+    private Button calculateLoanButton;
+    //These are the result labels
+    private TextView paymentEveryRateOnlyTextView;
+    private TextView totalPaymentsOnlyTextView;
+    //These are the result text views
+    private TextView paymentEveryRateTextView;
+    private TextView totalPaymentsTextView;
+    private TextView totalInterestTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
