1affe93025bd1288a8942eb7e3d94b6ddf162b68
Jay C <vikel965@hotmail.com>
2024-04-24 13:25:32 -0400

    Your commit message

--- app/src/main/java/com/example/simplemoneycalculator/MainActivity.java
+++ app/src/main/java/com/example/simplemoneycalculator/MainActivity.java
@@ -2,20 +2,49 @@ package com.example.simplemoneycalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Assigning variables
-    //Pick up here!
+    //Create variables
+    private Button loanCalculatorButton;
+    private Button savingCalculatorButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Assign variables
+        loanCalculatorButton = (Button) findViewById(R.id.loanCalculatorButton);
+        savingCalculatorButton = (Button) findViewById(R.id.savingCalculatorButton);
+
+
+        //Changing views from the MainActivity
+        //Check LoanCalculatorActivity.java to see proper implementation of onClickListener
+        loanCalculatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create new intent & then start that intent
+                Intent intent = new Intent(MainActivity.this, LoanCalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        savingCalculatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SavingsCalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
@@ -27,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         //Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_menu, menu);
-        //Fin the menu by ID
+        //Fin the menu items by ID
         MenuItem savedLoansMenuItem = (MenuItem) menu.findItem(R.id.savedLoansMenuItem);
         MenuItem savedSavingsMenuItem = (MenuItem) menu.findItem(R.id.savedSavingsMenuItem);
         return true;
@@ -38,10 +67,14 @@ public class MainActivity extends AppCompatActivity {
         //Handle the menu item selection
         switch (item.getItemId()){
             case R.id.savedLoansMenuItem:
-                //Execute code to show saved loans
+                //Create Intents like the onCreate method
+                Intent intent = new Intent(MainActivity.this, SavedLoansActivity.class);
+                startActivity(intent);
                 return true;
             case R.id.savedSavingsMenuItem:
                 //Execute code to show saved savings
+                Intent intent1 = new Intent(MainActivity.this, SavedSavingsActivity.class);
+                startActivity(intent1);
                 return true;
 
             default:
